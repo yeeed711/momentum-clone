73790c0e2acfaf1b601860c44506f921c72f9aab
yeeed711 <rladpwl910@naver.com>
2022-02-16 00:48:28 +0900

    text

--- js/background.js
+++ js/background.js
@@ -1,4 +1,4 @@
-const images = ["0.png", "1.png", "2.png"];
+/* const images = ["0.png", "1.png", "2.png"];
 
 const chosenImages = images[Math.floor(Math.random() * images.length)];
 
@@ -6,4 +6,4 @@ const bgImage = document.createElement("img");
 
 bgImage.src = `img/${chosenImages}`
 
-document.body.appendChild(bgImage);
+document.body.appendChild(bgImage); */
