546256fd587c145d5349ae1f1d17496a7cf0f2ff
yeeed711 <rladpwl910@naver.com>
2022-03-21 18:48:41 +0900

    new momentum
    시계 am.pm 추가
    링크 추가

--- js/clock.js
+++ js/clock.js
@@ -1,15 +1,16 @@
-'use strict'
+"use strict";
 
 const clock = document.querySelector(".clock");
 
-
-function getClock () {
-    const today = new Date();
-    const hours = String(today.getHours()).padStart(2,"0");
-    const minutes = String(today.getMinutes()).padStart(2,"0");
-    const seconds = String(today.getSeconds()).padStart(2,"0");
-    clock.innerText =(`${hours}:${minutes}:${seconds}`);
+function getClock() {
+  const today = new Date();
+  let hours = today.getHours();
+  const minutes = String(today.getMinutes()).padStart(2, "0");
+  const amPm = hours >= 12 ? "PM" : "AM";
+  hours = hours % 12;
+  hours = hours ? hours : 12;
+  clock.innerText = `${hours}:${minutes} ${amPm}`;
 }
 
 getClock();
-setInterval(getClock,1000);
+setInterval(getClock, 1000);
